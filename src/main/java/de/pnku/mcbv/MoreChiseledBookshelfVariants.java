40e198f5f81d4043dd905ee56ba6a3bef8ef5a77
pnk2u <159317511+pnk2u@users.noreply.github.com>
2025-02-10 04:46:15 +0100

    Update dep for rebase

--- src/main/java/de/pnku/mcbv/MoreChiseledBookshelfVariants.java
+++ src/main/java/de/pnku/mcbv/MoreChiseledBookshelfVariants.java
@@ -3,13 +3,19 @@ package de.pnku.mcbv;
 import de.pnku.mcbv.init.McbvBlockInit;
 import de.pnku.mcbv.init.McbvItemInit;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MoreChiseledBookshelfVariants implements ModInitializer {
     public static final String MODID = "lolmcbv";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+    public static boolean isNewerMcVersion = false;
 
     @Override
     public void onInitialize() {
+        isNewerMcVersion = FabricLoader.getInstance().getModContainer("minecraft").orElseThrow().getMetadata().getVersion().toString().equals("1.21.1");
         McbvBlockInit.registerBlocks();
         McbvItemInit.registerItems();
     }
--- src/main/java/de/pnku/mcbv/init/McbvBlockInit.java
+++ src/main/java/de/pnku/mcbv/init/McbvBlockInit.java
@@ -2,6 +2,7 @@ package de.pnku.mcbv.init;
 
 import de.pnku.mcbv.MoreChiseledBookshelfVariants;
 import de.pnku.mcbv.block.MoreChiseledBookShelfBlock;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityType;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.block.Block;
@@ -45,6 +46,8 @@ public class McbvBlockInit {
     private static void registerBlock(MoreChiseledBookShelfBlock chiseled_bookshelf) {
         Registry.register(BuiltInRegistries.BLOCK, MoreChiseledBookshelfVariants.asId(chiseled_bookshelf.chiseledBookshelfWoodType + "_chiseled_bookshelf"), chiseled_bookshelf);
         more_chiseled_bookshelves.add(chiseled_bookshelf);
-        BlockEntityType.CHISELED_BOOKSHELF.addSupportedBlock(chiseled_bookshelf);
+        if (MoreChiseledBookshelfVariants.isNewerMcVersion) {
+            ((FabricBlockEntityType) BlockEntityType.CHISELED_BOOKSHELF).addSupportedBlock(chiseled_bookshelf);
+        }
     }
 }
