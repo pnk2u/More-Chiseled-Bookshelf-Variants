12d658113ac81eba64b2f9e268beb9fb2af44824
pnku <pnku@pnku.de>
2026-03-18 01:23:01 +0100

    Update to 1.21.11 mapping

--- src/main/java/de/pnku/mcbv/MoreChiseledBookshelfVariants.java
+++ src/main/java/de/pnku/mcbv/MoreChiseledBookshelfVariants.java
@@ -4,7 +4,7 @@ import de.pnku.mcbv.init.McbvBlockInit;
 import de.pnku.mcbv.init.McbvItemInit;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +18,7 @@ public class MoreChiseledBookshelfVariants implements ModInitializer {
         McbvItemInit.registerItems();
     }
 
-    public static ResourceLocation asId(String path) {
-        return ResourceLocation.fromNamespaceAndPath(MODID, path);
+    public static Identifier asId(String path) {
+        return Identifier.fromNamespaceAndPath(MODID, path);
     }
 }
