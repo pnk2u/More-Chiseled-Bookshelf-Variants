3ed448621c4452281491e91958b3e8bed4f6e346
pnku <pnku@pnku.de>
2026-07-22 13:27:07 +0200

    Add Poplar Chiseled Bookshelf (`1.8.0-26.3:c1`)
    - Textures by @hunnycakehorse
    
    Co-Authored-By: hunnycakehorse <184146207+hunnycakehorse@users.noreply.github.com>

--- src/main/java/de/pnku/mcbv/init/McbvBlockInit.java
+++ src/main/java/de/pnku/mcbv/init/McbvBlockInit.java
@@ -22,6 +22,7 @@ public class McbvBlockInit {
     public static final MoreChiseledBookShelfBlock ACACIA_CHISELED_BOOKSHELF = new MoreChiseledBookShelfBlock(MapColor.COLOR_ORANGE, "acacia");
     public static final MoreChiseledBookShelfBlock DARK_OAK_CHISELED_BOOKSHELF = new MoreChiseledBookShelfBlock(MapColor.COLOR_BROWN, "dark_oak");
     public static final MoreChiseledBookShelfBlock PALE_OAK_CHISELED_BOOKSHELF = new MoreChiseledBookShelfBlock(MapColor.QUARTZ, "pale_oak");
+    public static final MoreChiseledBookShelfBlock POPLAR_CHISELED_BOOKSHELF = new MoreChiseledBookShelfBlock(MapColor.COLOR_LIGHT_GRAY, "poplar");
     public static final MoreChiseledBookShelfBlock MANGROVE_CHISELED_BOOKSHELF = new MoreChiseledBookShelfBlock(MapColor.COLOR_RED, "mangrove");
     public static final MoreChiseledBookShelfBlock CHERRY_CHISELED_BOOKSHELF = new MoreChiseledBookShelfBlock(MapColor.TERRACOTTA_WHITE, SoundType.CHERRY_WOOD, "cherry");
     public static final MoreChiseledBookShelfBlock BAMBOO_CHISELED_BOOKSHELF = new MoreChiseledBookShelfBlock(MapColor.COLOR_YELLOW, SoundType.BAMBOO_WOOD, "bamboo");
@@ -39,6 +40,7 @@ public class McbvBlockInit {
         registerBlock(ACACIA_CHISELED_BOOKSHELF);
         registerBlock(DARK_OAK_CHISELED_BOOKSHELF);
         registerBlock(PALE_OAK_CHISELED_BOOKSHELF);
+        registerBlock(POPLAR_CHISELED_BOOKSHELF);
         registerBlock(MANGROVE_CHISELED_BOOKSHELF);
         registerBlock(CHERRY_CHISELED_BOOKSHELF);
         registerBlock(BAMBOO_CHISELED_BOOKSHELF);
